e95dd4e3b78cf35f12b39663d788adf8fcccd4ed
Antony K Sebastian <antonyksebastian@antonys-air.home>
2026-02-15 18:27:57 +0000

    add auth util

---
+++ src/utils/authUtil.tsx
@@ -0,0 +1,13 @@
+export function getAuthHeaders() {
+  const token = localStorage.getItem("token");
+
+  const headers: Record<string, string> = {
+    "Content-Type": "application/json",
+  };
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return headers;
+}
