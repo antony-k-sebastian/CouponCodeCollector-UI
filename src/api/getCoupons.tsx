599dcdd8f1de7c812882d9f5505c9398a6225e78
Antony K Sebastian <antonyksebastian@antonys-air.home>
2026-02-17 20:15:25 +0000

    Add api to get private coupons

--- src/api/getCoupons.tsx
+++ src/api/getCoupons.tsx
@@ -1,4 +1,5 @@
 const Base_URL = import.meta.env.VITE_API_BASE_URL;
+import {getAuthHeaders} from "../utils/authUtil";
 
 export async function getPublicCoupons() {
   const response = await fetch(`${Base_URL}/api/coupons`, {
@@ -10,6 +11,19 @@ export async function getPublicCoupons() {
 
   if (!response.ok) {
     throw new Error("Failed to fetch coupons");
+  }
+
+  return response.json();
+}
+
+export async function getPrivateCoupons() {
+  const response = await fetch(`${Base_URL}/api/coupons/me`, {
+    method: "GET",
+    headers: getAuthHeaders(),
+  });
+
+  if (!response.ok){
+    throw new Error("Failed to fetch your coupons");
   }
 
   return response.json();
