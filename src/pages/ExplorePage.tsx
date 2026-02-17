d9a376d3a2268fb75b7cc3f1f471f5c36a3c2d3c
Antony K Sebastian <antonyksebastian@antonys-air.home>
2026-02-17 20:22:17 +0000

    Connect the logic with Explore page

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -3,24 +3,61 @@ import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 import CouponCard from "../components/CouponCard";
 import Footer from "../components/Footer";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import AuthModal from "../modals/AuthModal";
 import AddCouponModal from "../modals/AddCouponModal";
-import {getPublicCoupons} from "../api/publicCoupons";
+import { getPublicCoupons, getPrivateCoupons } from "../api/getCoupons";
+import { useAuth } from "../auth/authContext";
+
+type ViewMode = "public" | "private";
 
 export default function ExplorePage() {
   const [authOpen, setAuthOpen] = useState(false);
   const [openAddCoupon, setOpenAddCoupon] = useState(false);
   const [coupons, setCoupons] = useState([]);
+  const [view, setView] = useState<ViewMode>("public");
+
+  const { isLoggedIn } = useAuth();
+
   useEffect(() => {
-    getPublicCoupons().then(setCoupons).catch(console.error);
-  }, []);
+    const fetchCoupons = async () => {
+      try {
+        if (view == "public") {
+          const data = await getPublicCoupons();
+          setCoupons(data);
+          return;
+        }
+        if (!isLoggedIn) {
+          setCoupons([]);
+          return;
+        }
+        const data = await getPrivateCoupons();
+        setCoupons(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchCoupons();
+  }, [view, isLoggedIn]);
+
+  const handleExploreClick = () => setView("public");
+
+  const handleMyCouponClick = () => {
+    console.log("My Coupons clicked", { isLoggedIn });
+    if (!isLoggedIn) {
+      setAuthOpen(true);
+      return;
+    }
+    setView("private");
+  };
 
   return (
     <>
       <Header
         onLoginClick={() => setAuthOpen(true)}
         onAddCouponClick={() => setOpenAddCoupon(true)}
+        onMyCouponClick={handleMyCouponClick}
+        onExploreClick={handleExploreClick}
       />
       <AuthModal open={authOpen} onClose={() => setAuthOpen(false)} />
       <AddCouponModal
